97f95610de61daf578a1f5c9d8fbadd26a76842b
Ricardo Lima <ricardo.engp@gmail.com>
2025-03-09 15:36:58 -0300

    Adicionar roteamento com React Router e configurar rotas iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
 
-import { Button } from './components/ui/button'
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
